d4aada71f482e01d98fcb7fb56f03dc61a4ff5b7
NikKolesnikov <nikkolesnikov94@gmail.com>
2020-10-22 22:16:45 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let newStr = bracketsConfig.map(item => item.join(''));
+
+    for (let i = 0; i < newStr.length; i++) {
+        if (str.includes(newStr[i])) {
+            str = str.replace(newStr[i], '');
+            i = -1;
+        }
+    }
+
+    return (str.length === 0) ? true : false;
 }
